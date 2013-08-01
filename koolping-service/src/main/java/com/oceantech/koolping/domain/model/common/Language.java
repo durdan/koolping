8e9539e04576c7c29a97b54fc05e6b1d4692dce5
sanjoykroy <sanjoykr78@hotmail.com>
2013-08-01 23:32:29 +0100

    Movie related domain model is added.

---
+++ koolping-service/src/main/java/com/oceantech/koolping/domain/model/common/Language.java
@@ -0,0 +1,48 @@
+package com.oceantech.koolping.domain.model.common;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Language {
+    @Id
+    private String code;
+    private String description;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Language)) return false;
+
+        Language language = (Language) o;
+
+        if (code != null ? !code.equals(language.code) : language.code != null) return false;
+        if (description != null ? !description.equals(language.description) : language.description != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = code != null ? code.hashCode() : 0;
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+}
---
+++ koolping-service/src/main/java/com/oceantech/koolping/domain/model/movie/MovieTheater.java
@@ -0,0 +1,57 @@
+package com.oceantech.koolping.domain.model.movie;
+
+import com.oceantech.koolping.domain.model.common.Country;
+
+import javax.persistence.*;
+
+@Entity
+public class MovieTheater {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long theaterId;
+    private String name;
+    private String street;
+    private String town;
+    @OneToOne
+    private Country country;
+
+    public Long getTheaterId() {
+        return theaterId;
+    }
+
+    public void setTheaterId(Long theaterId) {
+        this.theaterId = theaterId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+}
